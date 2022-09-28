08384974491a4d877fc57b4498f66a9c576387f8
”Alexander <omgout.200@gmail.com>
2022-09-28 14:38:00 +0700

    small fix things, update, variables

--- server/app/controllers/api/v1/products_controller.rb
+++ server/app/controllers/api/v1/products_controller.rb
@@ -2,22 +2,28 @@ module Api
   module V1
     class ProductsController < ApplicationController
       def index
-        products = Product.all
-        render json: { products: products }, status: 200
+        @products = Product.all
+        render json: { products: @products }, status: 200
       end
 
       def create
-        product = Product.new(products_params)
-        if product.save
-          render json: product, status: 201
+        @product = Product.new(products_params)
+        if @product.save
+          render json: @product, status: 201
         else
-          render json: { message: product.errors.full_messages }, status: 400
+          render json: { message: @product.errors.full_messages }, status: 400
         end
       end
 
       def destroy
-        product = Product.find(products_params)
-        product.destroy
+        @product = Product.find(products_params)
+        @product.destroy
+      end
+
+      def update
+        if @product.update(products_params)
+          render json: { products: @products }, status: 200
+        end
       end
 
       private
