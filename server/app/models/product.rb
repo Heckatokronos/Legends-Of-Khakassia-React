bb1a808494b98efb4e7d741ac08b5d9b804015c6
”Alexander <omgout.200@gmail.com>
2022-09-28 21:47:13 +0700

    photo url, serializer things

--- server/app/models/product.rb
+++ server/app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ApplicationRecord
   has_one_attached :image
-  validates :title, presence: true
-  validates :description, presence: true
-  validates :price, presence: true
+
+  def image_url
+    Rails.application.routes.url_helpers.url_for(image) if image.attached?
+  end
 end
